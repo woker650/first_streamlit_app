61f1e7402b657a4fcbde79fcc970fbb50b5f8a7f
woker650 <110482678+woker650@users.noreply.github.com>
2022-08-02 18:09:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Blueberry oatmeal')
-streamlit.text('Spinach omlette')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
