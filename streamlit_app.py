b4320c80528b7e2983b4d744e787222dc40f4768
woker650 <110482678+woker650@users.noreply.github.com>
2022-08-02 18:07:25 +0200

    Update streamlit_app.py
    add header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Spinach omlette')
+streamlit.text('Hard-boiled Free-Range Egg')
