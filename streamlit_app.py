60ca0a456db80c30ede09724d369a75c45bbd64e
woker650 <110482678+woker650@users.noreply.github.com>
2022-08-02 18:10:19 +0200

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal🥣')
+streamlit.text('🥑🍞Kale, Spinach & Rocket Smoothie🥗')
+streamlit.text('Hard-Boiled Free-Range Egg🐔')   
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
